cc2c372ffb42d1468e68e2758673b7e1b57eda0a
RomeroFederico <romerof14@gmail.com>
2016-03-16 11:08:53 -0300

    El int.parse transforma string en numero solo si el string es un numero, de lo contrario lanza excepcion.

--- EjemplosClase1/Usodetrypass/Program.cs
+++ EjemplosClase1/Usodetrypass/Program.cs
@@ -9,6 +9,17 @@ namespace Usodetrypass
     {
         static void Main(string[] args)
         {
+            Console.Title = "Uso de tryparse";
+
+            string dato;
+
+            Console.WriteLine("Ingrese su edad: ");
+
+            dato = Console.ReadLine();
+
+            int numero;
+
+            numero = int.Parse(dato);
         }
     }
 }
