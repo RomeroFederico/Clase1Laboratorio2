7a7cef6702fa6cb09b83d0f731835844f27fded0
RomeroFederico <romerof14@gmail.com>
2016-03-16 10:19:00 -0300

    Primer commit
    Ejercitacion de GIT/GITHUB

---
+++ Clase1Ejercitacion1/Clase1Ejercitacion1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clase1Ejercitacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
